2a9d480a6b360872c3881cc289b4d2e923dc8330
oyeleyeoyebisi7-max <oyeleyeoyebisi7@gmail.com>
2026-05-14 10:17:44 +0100

    Update API endpoint from gemini-2.0 to gemini-1.5

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ Make it emotional and highly engaging.
 try{
 
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=}`,
 {
 method:"POST",
 headers:{
